921371d1a60aeadc2758ec6cacb681bb10cccde2
Temitope Akinsoto <takinsoto@gmail.com>
2021-02-03 13:45:54 +0100

    create sample component hooked up the the index.html file

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const Container = () => {
+    return (
+        <div>
+            <h1>This is the app container!</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<Container />, document.getElementById('root'));
