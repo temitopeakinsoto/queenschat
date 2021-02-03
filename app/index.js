88069770c498d2beb218caf09169a6b166c61706
Temitope Akinsoto <takinsoto@gmail.com>
2021-02-03 22:45:55 +0100

    Import <Header /> <Register/> and <Footer /> components respectively

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Register from "./components/Register";
 
 const Container = () => {
-    return (
-        <div>
-            <h1>This is the app container!</h1>
-        </div>
-    )
-}
+  return (
+    <>
+      <Header />
+      <Register />
+      <Footer />
+    </>
+  );
+};
 
-ReactDOM.render(<Container />, document.getElementById('root'));
+ReactDOM.render(<Container />, document.getElementById("root"));
