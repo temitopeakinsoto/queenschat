dc8b07ee71e0b8b09460e3394a4dca5e73ccddcc
Temitope Akinsoto <takinsoto@gmail.com>
2021-02-06 18:57:58 +0100

    save mongoDB connection string

--- app/index.js
+++ app/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Register from "./components/Register";
+import Terms from "./components/Terms";
+import About from "./components/About";
 
 const Container = () => {
   return (
     <>
       <Header />
-      <Register />
+      <Switch>
+        <Route path="/" exact component={Register} />
+        <Route path="/about-us" component={About} />
+        <Route path="/terms" component={Terms} />
+      </Switch>
       <Footer />
     </>
   );
